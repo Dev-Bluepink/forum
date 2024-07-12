99ce741b6a0b73303d3046fc0fd0ae0d9f2969e3
duoc <duoc6694@gmail.com>
2024-07-12 16:03:54 +0700

    log lỗi

--- src/app/utils/otpService.ts
+++ src/app/utils/otpService.ts
@@ -27,6 +27,8 @@ export function storeOTP(req: Request, email: string, otp: string): void {
     req.session.otps = {};
   }
   req.session.otps[email] = otpRecord;
+  console.log("đây là thông tin storeOTP");
+  console.log(req.session.otps);
 }
 
 // Hàm lấy OTP từ session
@@ -39,10 +41,17 @@ export function getOTP(req: Request, email: string): OTPRecord | null {
 
 // Hàm xác thực OTP
 export function verifyOTP(req: Request, email: string, otp: string): boolean {
+  console.log("đây là thông tin verifyOTP");
+  console.log(req.session.otps);
+  console.log(email);
+  console.log(otp);
   const otpRecord = getOTP(req, email);
+  console.log(otpRecord);
   if (otpRecord && req.session.otps) {
+    console.log("đây là thông tin verifyOTP true1");
     // Thêm kiểm tra req.session.otps
     if (otpRecord.otp === otp && otpRecord.expirationTime > Date.now()) {
+      console.log("đây là thông tin verifyOTP true2");
       delete req.session.otps[email];
       return true;
     }
